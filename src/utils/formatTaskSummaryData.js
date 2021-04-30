151c6a62aaeaf9ea92e022ec817865b73c51cc35
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2021-04-30 13:36:42 +0100

    Change formatTaskData argument to taskData

--- src/utils/formatTaskSummaryData.js
+++ src/utils/formatTaskSummaryData.js
@@ -1,48 +1,48 @@
 import moment from 'moment';
 import { LONG_DATE_FORMAT, SHORT_DATE_FORMAT } from '../constants';
 
-const formatTaskData = (taskSummaryData) => {
+const formatTaskData = (taskData) => {
   const account = {
     label: 'Account',
-    name: taskSummaryData?.organisations?.find(({ type }) => type === 'ORGACCOUNT')?.name || 'Unknown',
+    name: taskData?.organisations?.find(({ type }) => type === 'ORGACCOUNT')?.name || 'Unknown',
   };
   const arrival = {
     label: 'Arrival due',
-    location: taskSummaryData?.voyage?.arriveAt || '',
-    date: taskSummaryData?.arrivalTime ? moment(taskSummaryData?.arrivalTime).utc().format(LONG_DATE_FORMAT) : 'unknown',
-    description: (taskSummaryData?.voyage?.arriveAt ? `${taskSummaryData?.voyage?.arriveAt}` : 'unknown') + (taskSummaryData?.arrivalTime ? `, ${moment(taskSummaryData?.arrivalTime).utc().format(LONG_DATE_FORMAT)}` : ', unknown'),
-    fromNow: `${taskSummaryData?.arrivalTime ? `, ${moment(taskSummaryData?.arrivalTime).fromNow()}` : 'unknown'}`,
+    location: taskData?.voyage?.arriveAt || '',
+    date: taskData?.arrivalTime ? moment(taskData?.arrivalTime).utc().format(LONG_DATE_FORMAT) : 'unknown',
+    description: (taskData?.voyage?.arriveAt ? `${taskData?.voyage?.arriveAt}` : 'unknown') + (taskData?.arrivalTime ? `, ${moment(taskData?.arrivalTime).utc().format(LONG_DATE_FORMAT)}` : ', unknown'),
+    fromNow: `${taskData?.arrivalTime ? `, ${moment(taskData?.arrivalTime).fromNow()}` : 'unknown'}`,
   };
   const departure = {
     label: 'Departure',
-    location: taskSummaryData?.voyage?.departFrom || '',
-    date: taskSummaryData?.departureTime ? moment(taskSummaryData?.departureTime).utc().format(LONG_DATE_FORMAT) : 'unknown',
-    description: (taskSummaryData?.voyage?.departFrom ? `${taskSummaryData?.voyage?.departFrom}` : 'unknown') + (taskSummaryData?.departureTime ? `, ${moment(taskSummaryData?.departureTime).utc().format(LONG_DATE_FORMAT)}` : ', unknown'),
+    location: taskData?.voyage?.departFrom || '',
+    date: taskData?.departureTime ? moment(taskData?.departureTime).utc().format(LONG_DATE_FORMAT) : 'unknown',
+    description: (taskData?.voyage?.departFrom ? `${taskData?.voyage?.departFrom}` : 'unknown') + (taskData?.departureTime ? `, ${moment(taskData?.departureTime).utc().format(LONG_DATE_FORMAT)}` : ', unknown'),
   };
   const driver = {
-    dataExists: !!taskSummaryData?.people?.find(({ role }) => role === 'DRIVER'),
-    name: taskSummaryData?.people?.find(({ role }) => role === 'DRIVER')?.fullName || '',
-    dateOfBirth: taskSummaryData?.people?.find(({ role }) => role === 'DRIVER')?.dateOfBirth && moment(taskSummaryData?.people?.find(({ role }) => role === 'DRIVER')?.dateOfBirth).format(SHORT_DATE_FORMAT),
+    dataExists: !!taskData?.people?.find(({ role }) => role === 'DRIVER'),
+    name: taskData?.people?.find(({ role }) => role === 'DRIVER')?.fullName || '',
+    dateOfBirth: taskData?.people?.find(({ role }) => role === 'DRIVER')?.dateOfBirth && moment(taskData?.people?.find(({ role }) => role === 'DRIVER')?.dateOfBirth).format(SHORT_DATE_FORMAT),
   };
   const ferry = {
     label: 'Ferry',
-    description: taskSummaryData?.voyage?.description || '',
+    description: taskData?.voyage?.description || '',
   };
   const haulier = {
     label: 'Haulier',
-    name: taskSummaryData?.organisations?.find(({ type }) => type === 'ORGHAULIER')?.name || 'Unknown',
+    name: taskData?.organisations?.find(({ type }) => type === 'ORGHAULIER')?.name || 'Unknown',
   };
   const trailer = {
-    dataExists: taskSummaryData?.trailers?.length > 0,
-    label: taskSummaryData?.trailers?.length > 0 ? 'Trailer' : '',
-    registration: taskSummaryData?.trailers?.length > 0 && taskSummaryData?.trailers[0]?.registrationNumber ? taskSummaryData?.trailers[0]?.registrationNumber : '',
-    description: taskSummaryData?.trailers?.length > 0 && taskSummaryData?.trailers[0]?.description ? taskSummaryData?.trailers[0].description : 'no description',
+    dataExists: taskData?.trailers?.length > 0,
+    label: taskData?.trailers?.length > 0 ? 'Trailer' : '',
+    registration: taskData?.trailers?.length > 0 && taskData?.trailers[0]?.registrationNumber ? taskData?.trailers[0]?.registrationNumber : '',
+    description: taskData?.trailers?.length > 0 && taskData?.trailers[0]?.description ? taskData?.trailers[0].description : 'no description',
   };
   const vehicle = {
-    dataExists: taskSummaryData?.vehicles?.length > 0,
-    label: taskSummaryData?.vehicles?.length > 0 ? 'Vehicle' : '',
-    registration: taskSummaryData?.vehicles?.length > 0 && taskSummaryData?.vehicles[0]?.registrationNumber ? taskSummaryData?.vehicles[0].registrationNumber : '',
-    description: taskSummaryData?.vehicles?.length > 0 && taskSummaryData?.vehicles[0]?.description ? taskSummaryData?.vehicles[0].description : 'no description',
+    dataExists: taskData?.vehicles?.length > 0,
+    label: taskData?.vehicles?.length > 0 ? 'Vehicle' : '',
+    registration: taskData?.vehicles?.length > 0 && taskData?.vehicles[0]?.registrationNumber ? taskData?.vehicles[0].registrationNumber : '',
+    description: taskData?.vehicles?.length > 0 && taskData?.vehicles[0]?.description ? taskData?.vehicles[0].description : 'no description',
   };
 
   const formattedTaskSummary = {
