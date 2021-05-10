708cc2b05e1fe13c0e19b4539e875c8ad765c173
Boothiraj Palanisamy <bothees@gmail.com>
2021-05-10 16:01:51 +0100

    Add test to hide claim & unclaim button on task details page

--- cypress/integration/cerberus/task-details.spec.js
+++ cypress/integration/cerberus/task-details.spec.js
@@ -69,6 +69,20 @@ describe('Render tasks from Camunda and manage them on task details Page', () =>
     cy.get('.formio-component-note textarea').should('not.exist');
   });
 
+  it('Should hide Claim/UnClaim button for the tasks assigned to others', () => {
+    cy.getTasksAssignedToOtherUsers().then((tasks) => {
+      const processInstanceId = tasks.map(((item) => item.processInstanceId));
+      expect(processInstanceId.length).to.not.equal(0);
+      cy.intercept('GET', `/camunda/task?processInstanceId=${processInstanceId[0]}`).as('tasksDetails');
+      cy.visit(`/tasks/${processInstanceId[0]}`);
+      cy.wait('@tasksDetails').then(({ response }) => {
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+
+    cy.get('button.link-button').should('not.exist');
+  });
+
   it('Should Claim a task Successfully from task details page', () => {
     cy.intercept('POST', '/camunda/task/*/claim').as('claim');
 
