efd891df693a687736202348d39184b302a9ae35
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2021-06-04 17:24:46 +0100

    Add comment for task list sorting

--- src/routes/TaskLists/TaskListPage.jsx
+++ src/routes/TaskLists/TaskListPage.jsx
@@ -134,6 +134,12 @@ const TasksTab = ({ taskStatus, setError }) => {
           parsedTargetTaskSummariesValues = targetTaskSummaryValues;
         }
 
+        /*
+         * We initially grab the tasks from camunda in a sorted order (by 'due' asc)
+         * However after using the tasks data to query the variable endpoint we lose the
+         * sorting we had before. As a result, the amalgamation of /tasks and /variable api calls
+         * is sorted by the 'due' property to ensure the task list is in asc order
+        */
         setTargetTasks(parsedTargetTaskSummariesValues.sort((a, b) => new Date(a.due) - new Date(b.due)));
       } catch (e) {
         setError(e.message);
