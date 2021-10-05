61dc9e8249c43d6b9b1f8df46c9e6d2b981d793d
JenniKate Wallace <jennikate@gmail.com>
2021-10-05 12:21:04 +0100

    WIP redirect to task list

--- src/components/ClaimTaskButton.jsx
+++ src/components/ClaimTaskButton.jsx
@@ -31,6 +31,9 @@ const ClaimTaskButton = ({ assignee, taskId, setError = () => {}, businessKey, .
         history.go(0);
       }
     } catch (e) {
+      if (history.location.pathname !== `/tasks/${businessKey}`) {
+        history.push(`/tasks/${businessKey}`);
+      }
       setError(e.response.data.message);
       setAssignmentProgress(false);
     }
