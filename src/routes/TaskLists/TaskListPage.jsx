56fd88394ad22d6e894176b7e25352b5e829f3d7
JenniKate Wallace <jennikate@gmail.com>
2021-10-07 14:10:55 +0100

    Update topRisk

--- src/routes/TaskLists/TaskListPage.jsx
+++ src/routes/TaskLists/TaskListPage.jsx
@@ -189,7 +189,10 @@ const TasksTab = ({ taskStatus, setError }) => {
 
   const formatTargetRisk = (target) => {
     if (target.risks) {
-      const topRisk = target.risks[0].contents ? target.risks[0].contents.threatType : target.risks[0].name;
+      const topRisk = target.risks[0].contents
+        ? `SELECTOR: ${target.risks[0].contents.groupReference}, ${target.risks[0].contents.category}, ${target.risks[0].contents.threatType}`
+        : `${target.risks[0].name}, ${target.risks[0].rulePriority}, ${target.risks[0].abuseType}`;
+
       const count = target.risks.length - 1;
       return `${topRisk} and ${pluralise.withCount(count, '% other rule', '% other rules')}`;
     }
