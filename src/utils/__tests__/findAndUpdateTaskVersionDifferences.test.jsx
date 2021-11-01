c47e92c1a08006fd75744d8702b2853d922244df
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2021-11-01 16:07:09 +0000

    Rename test input argument to taskVersions and add assertion for no childSets

--- src/utils/__tests__/findAndUpdateTaskVersionDifferences.test.jsx
+++ src/utils/__tests__/findAndUpdateTaskVersionDifferences.test.jsx
@@ -1,10 +1,10 @@
 import findAndUpdateTaskVersionDifferences from '../findAndUpdateTaskVersionDifferences';
 
 describe('findAndUpdateTaskVersionDifferences', () => {
-  let input;
+  let taskVersions;
 
   beforeEach(() => {
-    input = [
+    taskVersions = [
       [
         {
           fieldSetName: 'Test',
@@ -82,30 +82,39 @@ describe('findAndUpdateTaskVersionDifferences', () => {
   });
 
   it('should add "-CHANGED" suffix to field type when a difference is present', () => {
-    findAndUpdateTaskVersionDifferences(input);
-    expect(input[0][0].contents[0].type).toBe('STRING-CHANGED');
-    expect(input[0][0].contents[1].type).toBe('INTEGER-CHANGED');
-    expect(input[0][0].childSets[0].contents[0].type).toBe('STRING');
-    expect(input[0][0].childSets[1].contents[0].type).toBe('STRING-CHANGED');
-    expect(input[1][0].contents[0].type).toBe('STRING');
-    expect(input[1][0].contents[1].type).toBe('INTEGER');
+    findAndUpdateTaskVersionDifferences(taskVersions);
+    expect(taskVersions[0][0].contents[0].type).toBe('STRING-CHANGED');
+    expect(taskVersions[0][0].contents[1].type).toBe('INTEGER-CHANGED');
+    expect(taskVersions[0][0].childSets[0].contents[0].type).toBe('STRING');
+    expect(taskVersions[0][0].childSets[1].contents[0].type).toBe('STRING-CHANGED');
+    expect(taskVersions[1][0].contents[0].type).toBe('STRING');
+    expect(taskVersions[1][0].contents[1].type).toBe('INTEGER');
   });
 
-  it('should handle mismatched lengthed contents arrays without error', () => {
-    input[1][0].contents = [];
+  it('should handle mismatched lengthed "contents" arrays without error', () => {
+    taskVersions[1][0].contents = [];
 
-    findAndUpdateTaskVersionDifferences(input);
+    findAndUpdateTaskVersionDifferences(taskVersions);
 
-    expect(input[0][0].contents[0].type).toBe('STRING-CHANGED');
-    expect(input[0][0].contents[1].type).toBe('INTEGER-CHANGED');
-    expect(input[0][0].childSets[0].contents[0].type).toBe('STRING');
-    expect(input[0][0].childSets[1].contents[0].type).toBe('STRING-CHANGED');
+    expect(taskVersions[0][0].contents[0].type).toBe('STRING-CHANGED');
+    expect(taskVersions[0][0].contents[1].type).toBe('INTEGER-CHANGED');
+    expect(taskVersions[0][0].childSets[0].contents[0].type).toBe('STRING');
+    expect(taskVersions[0][0].childSets[1].contents[0].type).toBe('STRING-CHANGED');
   });
 
   it('should return an object with correct "wasUpdated" and "differencesCounts" values', () => {
-    const { wasUpdated, differencesCounts } = findAndUpdateTaskVersionDifferences(input);
+    const { wasUpdated, differencesCounts } = findAndUpdateTaskVersionDifferences(taskVersions);
 
     expect(wasUpdated).toBe(true);
     expect(differencesCounts).toEqual([3, 0]);
+  });
+
+  it('should handle when there are no childSets', () => {
+    delete taskVersions[0][0].childSets;
+    delete taskVersions[1][0].childSets;
+    const { wasUpdated, differencesCounts } = findAndUpdateTaskVersionDifferences(taskVersions);
+
+    expect(wasUpdated).toBe(true);
+    expect(differencesCounts).toEqual([2, 0]);
   });
 });
