247dead83a1d94aa9cdeb16eb1bbbe1d82fe136b
Charles Okafor <ckokafor@hotmail.com>
2022-01-06 16:46:43 +0000

    Update task note test

--- src/routes/__tests__/TaskNotes.test.jsx
+++ src/routes/__tests__/TaskNotes.test.jsx
@@ -97,7 +97,8 @@ describe('TaskNotes', () => {
     });
 
     await waitFor(() => render(<TaskNotes displayForm businessKey="ghi" processInstanceId="123" />));
-    expect(screen.queryByText('Property delete changed from false to true')).toBeInTheDocument();
+
+    expect(screen.queryByText('testuser@email.com')).not.toBeInTheDocument();
   });
 
   it('should display develop the task activity', async () => {
