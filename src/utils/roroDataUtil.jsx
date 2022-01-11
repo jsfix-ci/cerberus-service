0424d7132a5f80f11f713ea09d444288a23cf495
Charles Okafor <ckokafor@hotmail.com>
2022-01-11 21:29:38 +0000

    Code clean up

--- src/utils/roroDataUtil.jsx
+++ src/utils/roroDataUtil.jsx
@@ -2,8 +2,7 @@ import generateTotalPassengers from './passengersGenerator';
 
 // Passengers data within roroData will be overwritten the new passeners count.
 const modify = (roroData) => {
-  const modifiedPassengers = generateTotalPassengers(roroData.driver, roroData.passengers);
-  roroData.passengers = modifiedPassengers;
+  roroData.passengers = generateTotalPassengers(roroData.driver, roroData.passengers);
   return roroData;
 };
 
