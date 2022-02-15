f30bdc3620461795b6d6432e2c79421be6ada472
Michael Ruocco <michael.ruocco@digital.homeoffice.gov.uk>
2022-02-15 15:27:15 +0000

    adds fixes for sections which were not showing entity search links correctly

--- src/routes/__tests__/TaskVersionsMode/SectionRenderer.test.jsx
+++ src/routes/__tests__/TaskVersionsMode/SectionRenderer.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import {
-  renderVersionSection,
+  renderBookingSection,
   renderVehicleSection,
   renderTrailerSection,
   renderOccupantsSection,
@@ -10,13 +10,13 @@ import {
 } from '../../TaskDetails/TaskVersionsMode/SectionRenderer';
 
 describe('SectionRenderer', () => {
-  describe('renderVersionSection', () => {
+  describe('renderBookingSection', () => {
     it('should return undefined if contents is undefined', () => {
       const input = {
         fieldSetName: 'field-set-name',
       };
 
-      const section = renderVersionSection(input);
+      const section = renderBookingSection(input);
 
       expect(section).toBeUndefined();
     });
@@ -27,7 +27,7 @@ describe('SectionRenderer', () => {
         contents: null,
       };
 
-      const section = renderVersionSection(input);
+      const section = renderBookingSection(input);
 
       expect(section).toBeUndefined();
     });
@@ -38,7 +38,7 @@ describe('SectionRenderer', () => {
         contents: [],
       };
 
-      const section = renderVersionSection(input);
+      const section = renderBookingSection(input);
 
       expect(section).toBeUndefined();
     });
@@ -64,7 +64,7 @@ describe('SectionRenderer', () => {
         ],
       };
 
-      const section = renderVersionSection(input);
+      const section = renderBookingSection(input);
 
       expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
         <div className="task-details-container bottom-border-thick">
@@ -92,16 +92,16 @@ describe('SectionRenderer', () => {
         fieldSetName: 'field-set-name',
         contents: [
           {
-            fieldName: 'Vehicle registration',
+            fieldName: 'Field 1',
             type: 'HIDDEN',
-            content: 'KP07YXG',
+            content: 'value 1',
             versionLastUpdated: null,
-            propName: 'registrationNumber',
+            propName: 'field1',
           },
         ],
       };
 
-      const section = renderVersionSection(input);
+      const section = renderBookingSection(input);
 
       expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
         <div className="task-details-container bottom-border-thick">
@@ -251,7 +251,7 @@ describe('SectionRenderer', () => {
             type: 'HIDDEN',
             content: 'KP07YXG',
             versionLastUpdated: null,
-            propName: 'trailerRegistrationNumber',
+            propName: 'registrationNumber',
           },
         ],
       };
@@ -262,6 +262,43 @@ describe('SectionRenderer', () => {
         <div className="task-details-container bottom-border-thick">
           <h3 className="title-heading">Vehicle</h3>
           <div className="govuk-task-details-grid-column" />
+        </div>,
+      ));
+    });
+
+    it('should render entity search link if registration number and link field are present', () => {
+      const input = {
+        contents: [
+          {
+            fieldName: 'Vehicle registration',
+            type: 'STRING',
+            content: 'KP07YXG',
+            versionLastUpdated: null,
+            propName: 'registrationNumber',
+          },
+          {
+            fieldName: 'Entity Search URL',
+            type: 'HIDDEN',
+            content: 'http://localhost:4020/search?term=1234567890&type=VEHICLE&fields=["id"]',
+            versionLastUpdated: null,
+            propName: 'vehicleEntitySearchUrl',
+          },
+        ],
+      };
+
+      const section = renderVehicleSection(input, 'RORO_TOURIST');
+
+      expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
+        <div className="task-details-container bottom-border-thick">
+          <h3 className="title-heading">Vehicle</h3>
+          <div className="govuk-task-details-grid-column">
+            <div className="">
+              <ul>
+                <li className="govuk-grid-key font__light"><span className="govuk-grid-key font__light">Vehicle registration</span></li>
+                <li className="govuk-grid-value font__bold"><a href="http://localhost:4020/search?term=1234567890&type=VEHICLE&fields=[&quot;id&quot;]" target="_blank" rel="noreferrer noopener">KP07YXG</a></li>
+              </ul>
+            </div>
+          </div>
         </div>,
       ));
     });
@@ -410,6 +447,43 @@ describe('SectionRenderer', () => {
         </div>,
       ));
     });
+
+    it('should render entity search link if trailer registration number and link field are present', () => {
+      const input = {
+        contents: [
+          {
+            fieldName: 'Trailer registration number',
+            type: 'STRING',
+            content: 'LQ18ZYH',
+            versionLastUpdated: null,
+            propName: 'trailerRegistrationNumber',
+          },
+          {
+            fieldName: 'Entity Search URL',
+            type: 'HIDDEN',
+            content: 'http://localhost:4020/search?term=1212121212&type=VEHICLE&fields=["id"]',
+            versionLastUpdated: null,
+            propName: 'trailerEntitySearchUrl',
+          },
+        ],
+      };
+
+      const section = renderTrailerSection(input, 'RORO_ACCOMPANIED_FREIGHT');
+
+      expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
+        <div className="task-details-container bottom-border-thick">
+          <h3 className="title-heading">Trailer</h3>
+          <div className="govuk-task-details-grid-column">
+            <div className="">
+              <ul>
+                <li className="govuk-grid-key font__light"><span className="govuk-grid-key font__light">Trailer registration number</span></li>
+                <li className="govuk-grid-value font__bold"><a href="http://localhost:4020/search?term=1212121212&type=VEHICLE&fields=[&quot;id&quot;]" target="_blank" rel="noreferrer noopener">LQ18ZYH</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>,
+      ));
+    });
   });
 
   describe('renderOccupantsSection', () => {
@@ -475,6 +549,59 @@ describe('SectionRenderer', () => {
               <ul>
                 <li className="govuk-grid-key font__light"><><span className="govuk-grid-key font__light">Name</span></></li>
                 <li className="govuk-grid-value font__bold">MRS SECOND PASSENGER</li>
+              </ul>
+            </div>
+          </div>
+        </div>,
+      ));
+    });
+
+    it('should return second passenger entity search link if fields if present', () => {
+      const input = {
+        fieldSetName: 'Passengers',
+        childSets: [
+          {
+            contents: [
+              {
+                fieldName: 'Name',
+                type: 'STRING',
+                content: 'MR FIRST PASSENGER',
+                versionLastUpdated: null,
+                propName: 'name',
+              },
+            ],
+          },
+          {
+            contents: [
+              {
+                fieldName: 'Name',
+                type: 'STRING',
+                content: 'MRS SECOND PASSENGER',
+                versionLastUpdated: null,
+                propName: 'name',
+              },
+              {
+                fieldName: 'Entity Search URL',
+                type: 'HIDDEN',
+                content: 'http://localhost:4020/search?term=98989898&type=PERSON&fields=["id"]',
+                versionLastUpdated: null,
+                propName: 'entitySearchUrl',
+              },
+            ],
+          },
+        ],
+      };
+
+      const section = renderOccupantsSection(input, 'any-icon');
+
+      expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
+        <div className="task-details-container">
+          <h3 className="title-heading">{input.fieldSetName}</h3>
+          <div className="govuk-task-details-grid-column">
+            <div className="govuk-task-details-grid-item">
+              <ul>
+                <li className="govuk-grid-key font__light"><><span className="govuk-grid-key font__light">Name</span></></li>
+                <li className="govuk-grid-value font__bold"><a href="http://localhost:4020/search?term=98989898&type=PERSON&fields=[&quot;id&quot;]" target="_blank" rel="noreferrer noopener">MRS SECOND PASSENGER</a></li>
               </ul>
             </div>
           </div>
@@ -592,6 +719,87 @@ describe('SectionRenderer', () => {
                   <ul>
                     <li className="govuk-grid-key font__light"><span className="govuk-grid-key font__light">Name</span></li>
                     <li className="govuk-grid-value font__bold">MR OTHER PASSENGER</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </details>
+        </div>,
+      ));
+    });
+
+    it('should return other passenger entity search link if fields if present', () => {
+      const input = {
+        fieldSetName: 'Passengers',
+        childSets: [
+          {
+            contents: [
+              {
+                fieldName: 'Name',
+                type: 'STRING',
+                content: 'MR FIRST PASSENGER',
+                versionLastUpdated: null,
+                propName: 'name',
+              },
+            ],
+          },
+          {
+            contents: [
+              {
+                fieldName: 'Name',
+                type: 'STRING',
+                content: 'MRS SECOND PASSENGER',
+                versionLastUpdated: null,
+                propName: 'name',
+              },
+            ],
+          },
+          {
+            contents: [
+              {
+                fieldName: 'Name',
+                type: 'STRING',
+                content: 'MR OTHER PASSENGER',
+                versionLastUpdated: null,
+                propName: 'name',
+              },
+              {
+                fieldName: 'Entity Search URL',
+                type: 'HIDDEN',
+                content: 'http://localhost:4020/search?term=56565656&type=PERSON&fields=["id"]',
+                versionLastUpdated: null,
+                propName: 'entitySearchUrl',
+              },
+            ],
+          },
+        ],
+      };
+
+      const section = renderOccupantsSection(input, 'any-icon');
+
+      expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
+        <div className="task-details-container">
+          <h3 className="title-heading">{input.fieldSetName}</h3>
+          <div className="govuk-task-details-grid-column">
+            <div className="govuk-task-details-grid-item">
+              <ul>
+                <li className="govuk-grid-key font__light"><><span className="govuk-grid-key font__light">Name</span></></li>
+                <li className="govuk-grid-value font__bold">MRS SECOND PASSENGER</li>
+              </ul>
+            </div>
+          </div>
+          <details className="govuk-details" data-module="govuk-details">
+            <summary className="govuk-details__summary">
+              <span className="govuk-details__summary-text">
+                Show more
+              </span>
+            </summary>
+            <div className="govuk-hidden-passengers">
+              <div className="govuk-task-details-grid-column">
+                <div className="govuk-task-details-grid-item">
+                  <ul>
+                    <li className="govuk-grid-key font__light"><><span className="govuk-grid-key font__light">Name</span></></li>
+                    <li className="govuk-grid-value font__bold"><a href="http://localhost:4020/search?term=56565656&type=PERSON&fields=[&quot;id&quot;]" target="_blank" rel="noreferrer noopener">MR OTHER PASSENGER</a></li>
                   </ul>
                 </div>
               </div>
@@ -782,7 +990,7 @@ describe('SectionRenderer', () => {
       expect(section).toBeUndefined();
     });
 
-    it('should render traveller and document fields is icon is single tourist icon', () => {
+    it('should render traveller and document fields if icon is single tourist icon', () => {
       const input = {
         childSets: [
           {
@@ -974,6 +1182,47 @@ describe('SectionRenderer', () => {
               <ul>
                 <li className="govuk-grid-key font__light"><span className="govuk-grid-key font__light">Nationality</span></li>
                 <li className="govuk-grid-value font__bold">GB</li>
+              </ul>
+            </div>
+          </div>
+        </div>,
+      ));
+    });
+
+    it('should render primary traveller entity search link if fields are present', () => {
+      const input = {
+        childSets: [
+          {
+            contents: [
+              {
+                fieldName: 'Name',
+                type: 'STRING',
+                content: 'MR PRIMARY TRAVELLER',
+                versionLastUpdated: null,
+                propName: 'name',
+              },
+              {
+                fieldName: 'Entity Search URL',
+                type: 'HIDDEN',
+                content: 'http://localhost:4020/search?term=2828282828&type=PERSON&fields=["id"]',
+                versionLastUpdated: null,
+                propName: 'entitySearchUrl',
+              },
+            ],
+          },
+        ],
+      };
+
+      const section = renderPrimaryTraveller(input, 'any-icon');
+
+      expect(ReactDOMServer.renderToString(section)).toEqual(ReactDOMServer.renderToString(
+        <div className="task-details-container bottom-border-thick">
+          <h3 className="title-heading">Primary Traveller</h3>
+          <div className="govuk-task-details-grid-column">
+            <div className="">
+              <ul>
+                <li className="govuk-grid-key font__light"><span className="govuk-grid-key font__light">Name</span></li>
+                <li className="govuk-grid-value font__bold"><a href="http://localhost:4020/search?term=2828282828&type=PERSON&fields=[&quot;id&quot;]" target="_blank" rel="noreferrer noopener">MR PRIMARY TRAVELLER</a></li>
               </ul>
             </div>
           </div>
