0c2a37c942dab8b820fbe3e315a86ca291b2e153
JenniKate Wallace <jennikate@gmail.com>
2022-04-11 11:10:08 +0100

    Create placeholder pages for airpax

---
+++ src/routes/airpax/TaskDetails/TaskDetailsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TaskDetailsPage = () => {
+  return (
+    <h1>Temp AirPax Page for Task Details</h1>
+  );
+};
+
+export default TaskDetailsPage;
---
+++ src/routes/airpax/TaskLists/TaskListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TaskListPage = () => {
+  return (
+    <h1>Temp AirPax Page for Task List</h1>
+  );
+};
+
+export default TaskListPage;
