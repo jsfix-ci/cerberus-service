190f47aacf2d35f19660bf4e2846e42c3f2ab504
Shakir Khan <shakirkhan02@gmail.com>
2022-04-20 15:33:11 +0100

    COP-10471: Task detials Version header structure

---
+++ src/routes/airpax/TaskDetails/TaskVersions.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import * as pluralise from 'pluralise';
+// Config
+import { LONG_DATE_FORMAT } from '../../../constants';
+// Components/govuk
+import Accordion from '../../../govuk/Accordion';
+
+const TaskVersions = ({ taskVersions, businessKey }) => {
+  dayjs.extend(utc);
+  return (
+    <Accordion
+      className="task-versions"
+      id={`task-versions-${businessKey}`}
+      items={
+        taskVersions.map((version, index) => {
+          const threatLevel = version.risks.highestThreatLevel;
+          return {
+            expanded: index === 0,
+            heading: `Version ${version.number} ${index === 0 ? '(latest)' : ''}`,
+            summary: (
+              <>
+                <div className="task-versions--left">
+                  <div className="govuk-caption-m">{dayjs.utc(version.createdAt ? version.createdAt : null).local().format(LONG_DATE_FORMAT)}</div>
+                </div>
+                <div className="task-versions--right">
+                  <ul className="govuk-list">
+                    <li>{pluralise.withCount(version.changes, '% change', '% changes', 'No changes')} in this version</li>
+                    {threatLevel?.type === 'RULE' && <li>Highest threat level is <span className="govuk-body govuk-tag govuk-tag--positiveTarget">{threatLevel.value}</span></li>}
+                    {threatLevel?.type === 'SELECTOR' && <li>Highest threat level is <span className="govuk-body govuk-tag govuk-tag--positiveTarget">Category {threatLevel.value}</span></li>}
+                    {!threatLevel && <li>No rule matches</li>}
+                  </ul>
+                </div>
+              </>
+            ),
+            children: null,
+          };
+        })
+        }
+    />
+
+  );
+};
+
+export default TaskVersions;
