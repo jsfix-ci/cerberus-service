3c7311b3b4dca41c0a9ca6e478271c75690dcdb1
Charles Okafor <ckokafor@hotmail.com>
2022-04-27 15:20:16 +0100

    WIP - added payment details

--- src/routes/airpax/TaskDetails/builder/Booking.jsx
+++ src/routes/airpax/TaskDetails/builder/Booking.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import { LONG_DATE_FORMAT } from '../../../../constants';
+import { BookingUtil, PersonUtil, DateTimeUtil, MovementUtil } from '../../utils';
 
-const Booking = ({ booking }) => {
+import calculateTimeDifference from '../../../../utils/calculateDatetimeDifference';
+
+const toBookingTimeDiference = (booking, version) => {
+  const journey = MovementUtil.movementJourney(version);
+  const departureTime = MovementUtil.departureTime(journey);
+  const dateTimeList = DateTimeUtil.toList(BookingUtil.bookedAt(booking), departureTime);
+  return calculateTimeDifference(dateTimeList).replace('ago', 'before departure');
+};
+
+const toPayments = (booking) => {
+  const payments = BookingUtil.payments(booking);
+  if (payments) {
+    return payments.map((payment, index) => {
+      return (
+        <div key={index} className="thin-border">
+          <span className="font__bold">{BookingUtil.paymentAmount(payment)}</span>
+          <br />
+          <span className="font__regular">Credit card ending {BookingUtil.cardLastFourDigits(payment)}, expiry </span>
+        </div>
+      );
+    });
+  }
+};
+
+const Booking = ({ booking, ticket, version }) => {
   return (
     <div className="task-details-container">
       <h3 className="title-heading airpax-title-heading">Booking</h3>
@@ -8,46 +34,55 @@ const Booking = ({ booking }) => {
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Reference</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">{BookingUtil.bookingRef(booking)}</li>
           </ul>
         </div>
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Number of travellers</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">{PersonUtil.totalPersons(version)}</li>
           </ul>
         </div>
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Booking date</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">
+              {DateTimeUtil.format(BookingUtil.bookedAt(booking), LONG_DATE_FORMAT)}
+            </li>
+            <li className="govuk-grid-value font__light">{toBookingTimeDiference(booking, version)}</li>
           </ul>
         </div>
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Booking country</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">
+              {BookingUtil.countryName(booking)} ({BookingUtil.countryCode(booking)})
+            </li>
           </ul>
         </div>
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Booking type</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">{BookingUtil.bookingType(booking)}</li>
           </ul>
         </div>
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Ticket number</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">{BookingUtil.ticketNumber(ticket)}</li>
           </ul>
         </div>
         <div>
           <ul>
             <li className="govuk-grid-key font__light">Ticket type</li>
-            <li className="govuk-grid-value font__bold">_Placeholder_</li>
+            <li className="govuk-grid-value font__bold">{BookingUtil.ticketType(ticket)}</li>
           </ul>
         </div>
       </div>
+      <div className="thin-border">
+        <span>Payments</span>
+      </div>
+      {toPayments(booking)}
     </div>
   );
 };
--- src/routes/airpax/utils/bookingUtil.js
+++ src/routes/airpax/utils/bookingUtil.js
@@ -1,7 +1,91 @@
+import lookup from 'country-code-lookup';
 import { getFormattedDate, toDateTimeList } from './datetimeUtil';
 import calculateTimeDifference from '../../../utils/calculateDatetimeDifference';
 
 import { UNKNOWN_TEXT, SHORT_DATE_FORMAT_ALT, STANDARD_HOUR_MINUTE_FORMAT } from '../../../constants';
+
+const hasPaymentCard = (payment) => {
+  return !!payment.card;
+};
+
+const getPaymentCard = (payment) => {
+  if (hasPaymentCard(payment)) {
+    return payment.card;
+  }
+  return null;
+};
+
+const getCardLastFourDigits = (payment) => {
+  const paymentCard = getPaymentCard(payment);
+  if (paymentCard) {
+    return paymentCard.number.substr(paymentCard.number.length - 4);
+  }
+  return UNKNOWN_TEXT;
+};
+
+const getPaymentAmount = (payment) => {
+  if (!payment?.amount) {
+    return UNKNOWN_TEXT;
+  }
+  return payment.amount;
+};
+
+const hasPayments = (booking) => {
+  return !!booking.payments;
+};
+
+const getPayments = (booking) => {
+  if (hasPayments(booking)) {
+    return booking.payments;
+  }
+  return null;
+};
+
+const getTicketType = (ticket) => {
+  if (!ticket?.type) {
+    return UNKNOWN_TEXT;
+  }
+  return ticket.type;
+};
+
+const getTicketNumber = (ticket) => {
+  if (!ticket?.number) {
+    return UNKNOWN_TEXT;
+  }
+  return ticket.number;
+};
+
+const hasTicket = (booking) => {
+  return !!booking.ticket;
+};
+
+const getTicket = (booking) => {
+  if (hasTicket(booking)) {
+    return booking.ticket;
+  }
+  return null;
+};
+
+const getBookingType = (booking) => {
+  if (!booking?.type) {
+    return UNKNOWN_TEXT;
+  }
+  return booking.type;
+};
+
+const getBookingCountryCode = (booking) => {
+  if (!booking?.country) {
+    return UNKNOWN_TEXT;
+  }
+  return booking.country;
+};
+
+const getBookingCountryName = (booking) => {
+  if (getBookingCountryCode(booking) === UNKNOWN_TEXT) {
+    return UNKNOWN_TEXT;
+  }
+  return lookup.byIso(getBookingCountryCode(booking)).country;
+};
 
 const toBookingDateText = (booking) => {
   if (!booking?.bookedAt) {
@@ -52,6 +136,16 @@ const BookingUtil = {
   toCheckInText: toCheckInTimeText,
   bookingRef: getBookingReference,
   toBookingText: toBookingDateText,
+  countryCode: getBookingCountryCode,
+  countryName: getBookingCountryName,
+  bookingType: getBookingType,
+  bookingTicket: getTicket,
+  ticketNumber: getTicketNumber,
+  ticketType: getTicketType,
+  payments: getPayments,
+  paymentAmount: getPaymentAmount,
+  paymentCard: getPaymentCard,
+  cardLastFourDigits: getCardLastFourDigits,
 };
 
 export default BookingUtil;
@@ -63,4 +157,14 @@ export {
   getBooking,
   getBookedAt,
   getBookedPriorToDeparture,
+  getBookingCountryCode,
+  getBookingCountryName,
+  getBookingType,
+  getTicket,
+  getTicketNumber,
+  getTicketType,
+  getPayments,
+  getPaymentAmount,
+  getPaymentCard,
+  getCardLastFourDigits,
 };
