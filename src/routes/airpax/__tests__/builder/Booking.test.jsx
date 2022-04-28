24d0da66ac357ef3578ab368f35db1ff96517d36
Charles Okafor <ckokafor@hotmail.com>
2022-04-28 09:31:28 +0100

    adds tests for booking component & task versions

---
+++ src/routes/airpax/__tests__/builder/Booking.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Booking from '../../TaskDetails/builder/Booking';
+import taskDetailsData from '../../__fixtures__/taskData_AirPax_TaskDetails.fixture.json';
+
+describe('Booking', () => {
+  it('should render the booking component', () => {
+    const tree = renderer.create(<Booking version={taskDetailsData.versions[1]} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
