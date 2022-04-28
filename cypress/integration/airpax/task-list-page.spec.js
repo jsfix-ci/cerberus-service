9f5ff86ce3ac1516e2b536a6ad179a226e2e8b77
mitchelegboko <mitchel.egboko@identitye2e.com>
2022-04-28 13:03:23 +0100

    fixed lint errors

--- cypress/integration/airpax/task-list-page.spec.js
+++ cypress/integration/airpax/task-list-page.spec.js
@@ -5,17 +5,17 @@ describe(' Airpax task list page', () => {
   it('Should verify task container list for airpax/tasks', () => {
     cy.visit('/airpax/tasks');
     const taskNavigationItems = [
-                                  'New',
-                                  'In progress',
-                                  'Issued',
-                                  'Complete',
+      'New',
+      'In progress',
+      'Issued',
+      'Complete',
     ];    
     cy.get('.govuk-heading-xl').invoke('text').then((Heading) => {
       expect(Heading).to.equal('Task management');
     });
     cy.get('.govuk-tabs__list li a').each((navigationItem, index) => {
-    cy.wrap(navigationItem).click()
-    .should('contain.text', taskNavigationItems[index]).and('be.visible');
+      cy.wrap(navigationItem).click()
+        .should('contain.text', taskNavigationItems[index]).and('be.visible');
     });
   });
 
