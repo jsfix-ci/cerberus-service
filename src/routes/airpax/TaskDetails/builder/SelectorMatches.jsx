1953700797f7246e5dcd23d3ba6f2d901330ea8c
Shakir Khan <shakirkhan02@gmail.com>
2022-05-05 09:56:40 +0100

    COP-10599: created a component for selectors

---
+++ src/routes/airpax/TaskDetails/builder/SelectorMatches.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Tab, TabList, Tabs, TabPanel } from 'react-tabs';
+import '../../../../__assets__/ReactTabs.scss';
+
+const SelectorMatches = ({ version }) => {
+  console.log("VERSIONS ", version);
+  return (
+    <div>
+      <p>Shakir</p>
+    </div>
+  );
+};
+
+export default SelectorMatches;
