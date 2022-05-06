e3256904b13953c36cdbffcac02fbbf85a275f06
Charles Okafor <ckokafor@hotmail.com>
2022-05-06 16:19:04 +0100

    wip - adds itinerary section

--- src/routes/airpax/TaskDetails/builder/Itinerary.jsx
+++ src/routes/airpax/TaskDetails/builder/Itinerary.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { MovementUtil } from '../../utils';
 
-const Itinerary = () => {
+const Itinerary = ({ version }) => {
+  const itinerary = MovementUtil.movementItinerary(MovementUtil.movementJourney(version));
+  const itineraryBlocks = MovementUtil.itineraryBlock(itinerary);
   return (
     <div className="task-details-container">
       <h3 className="title-heading airpax-title-heading">Itinerary</h3>
-      <div className="govuk-task-details-grid-column" />
+      <div className="thin-border" />
+      {itineraryBlocks.map((itineraryBlock, index) => {
+        return (
+          <div key={index} className={`${index !== itineraryBlock.length - 1 && 'thin-border'} govuk-!-margin-top-1`}>
+            {itineraryBlock}
+          </div>
+        );
+      })}
     </div>
   );
 };
