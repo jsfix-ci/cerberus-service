2b26347ceb8ce086ed17a845c1c5eda131a019a2
mitchelegboko <mitchel.egboko@identitye2e.com>
2022-06-07 22:51:29 +0100

    COP-10838 Update departure status displayed on task list page

--- cypress/integration/airpax/task-list-page.spec.js
+++ cypress/integration/airpax/task-list-page.spec.js
@@ -1,10 +1,13 @@
+import { find } from "lodash";
+
 describe('Airpax task list page', () => {
   before(() => {
     cy.login(Cypress.env('userName'));
     cy.acceptPNRTerms();
+  //  cy.visit('/airpax/tasks');
   });
   it('Should verify task container list for airpax/tasks', () => {
-    cy.visit('/airpax/tasks');
+   // cy.visit('/airpax/tasks');
     const taskNavigationItems = [
       'New',
       'In progress',
@@ -20,13 +23,79 @@ describe('Airpax task list page', () => {
   });
 
   it('Should verify /v2/targeting-tasks/pages returns with status code 200', () => {
-    cy.visit('/airpax/tasks');
+  //  cy.visit('/airpax/tasks');
     cy.intercept('POST', '/v2/targeting-tasks/pages').as('taskList');
     cy.wait('@taskList').then(({ response }) => {
       expect(response.statusCode).to.equal(200);
       cy.get('.card-container').should('be.visible');
     });
   });
+
+  it('Should display departure status in task list page', () => {
+    const statusInitials = ['CI', 'BP', 'DC', 'DX'];
+    const status = ['CHECKED_IN', 'BOOKED_PASSENGER', 'DEPARTURE_CONFIRMED', 'DEPARTURE_EXCEPTION'];
+    statusInitials.forEach(function (statusInitials, i) {
+      cy.intercept('POST', '/v2/targeting-tasks/pages').as('taskList');
+      const taskName = 'AIRPAX';
+      cy.fixture('airpax/task-airpax.json').then((task) => {
+        task.data.movementId = `${taskName}_${Math.floor((Math.random() * 1000000) + 1)}:CMID=TEST`;
+        task.data.movement.serviceMovement.features.feats['STANDARDISED:lastReportedStatus'].value = statusInitials; 
+        console.log(task);
+          cy.createAirPaxTask(task).then((response) => {
+            expect(response.movement.id).to.contain('AIRPAX');
+           expect(response.movement.flight.departureStatus).to.contain(status[i]);
+            console.log(response);
+            let businessKey = response.id;
+            cy.visit('/airpax/tasks');
+            cy.wait(3000);
+            cy.wait('@taskList').then(({ response }) => {
+              expect(response.statusCode).to.equal(200);
+            });
+            cy.get('.govuk-task-list-card').each((item) => {
+              cy.wrap(item).find('h4.task-heading').invoke('text').then((text) => {
+                cy.log('task text', text);
+                if (text.includes(businessKey)) {
+                  cy.wrap(item).find('.hods-tag').invoke('text').then((text) => {
+                    expect(text).to.equal(statusInitials)
+                  });
+                } 
+              });
+            });
+          });
+      });
+    });
+  });
+
+  it('Should not display departure status in task list page if departure status is null', () => {
+    cy.intercept('POST', '/v2/targeting-tasks/pages').as('taskList');
+    const taskName = 'AIRPAX';
+    cy.fixture('airpax/task-airpax.json').then((task) => {
+      task.data.movementId = `${taskName}_${Math.floor((Math.random() * 1000000) + 1)}:CMID=TEST`;
+      task.data.movement.serviceMovement.features.feats['STANDARDISED:lastReportedStatus'].value = null;
+      console.log(task);
+      cy.createAirPaxTask(task).then((response) => {
+        expect(response.movement.id).to.contain('AIRPAX');
+        expect(response.movement.flight.departureStatus).to.eql(null);
+        console.log(response);
+        let businessKey = response.id;
+        cy.visit('/airpax/tasks');
+        cy.wait(3000);
+        cy.wait('@taskList').then(({ response }) => {
+          expect(response.statusCode).to.equal(200);
+        });
+        cy.get('.govuk-task-list-card').each((item) => {
+          cy.wrap(item).find('h4.task-heading').invoke('text').then((text) => {
+            cy.log('task text', text);
+            if (text.includes(businessKey)) {
+              cy.wrap(item).find('.hods-tag').should('not.exist');
+            }
+          });
+        });
+      });
+    });
+  });
+
+
 
   after(() => {
     cy.contains('Sign out').click();
