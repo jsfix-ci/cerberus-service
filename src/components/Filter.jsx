94a4703afba5f0528f73e3e6a1011aea4bd73978
Charles Okafor <ckokafor@hotmail.com>
2022-06-09 16:54:18 +0100

    initial commit

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,155 +1,171 @@
 import FormRenderer, { Utils } from '@ukhomeoffice/cop-react-form-renderer';
+import { MultiSelectAutocomplete } from '@ukhomeoffice/cop-react-components';
 import React, { useState, useCallback, useEffect } from 'react';
 import { DEFAULT_APPLIED_RORO_FILTER_STATE, MOVEMENT_VARIANT } from '../constants';
+
 import { airpax, roro } from '../cop-forms/filter';
-
-const getCountForOption = (fieldId, value, taskStatus, movementModeCounts, selectorCounts) => {
-  if (fieldId === 'hasSelectors') {
-    if (value === DEFAULT_APPLIED_RORO_FILTER_STATE.hasSelectors) {
-      value = null;
-    }
-    return selectorCounts?.find((f) => {
-      return f.filterParams[fieldId] === JSON.parse(value);
-    })?.statusCounts[taskStatus];
-  }
-  if (fieldId === 'selectors') {
-    return selectorCounts?.find((f) => {
-      return f.filterParams[fieldId] === value;
-    })?.statusCounts[taskStatus];
-  }
-  if (fieldId === 'mode') {
-    if (!value) {
-      return null;
-    }
-    return movementModeCounts?.find((f) => {
-      return f.filterParams.movementModes[0] === value;
-    })?.statusCounts[taskStatus];
-  }
-  return 0;
-};
-
-const setupOptions = (component, taskStatus, movementModeCounts, selectorCounts) => {
-  return component.data.options.map((opt) => {
-    let count = getCountForOption(component.fieldId, opt.value, taskStatus, movementModeCounts, selectorCounts);
-    count = count === undefined ? 0 : count;
-    const originalLabel = opt.originalLabel || opt.label;
-    const label = count === null ? originalLabel : `${originalLabel} (${count})`;
-    return {
-      ...opt,
-      count,
-      originalLabel,
-      label,
-    };
-  });
-};
-
-const setupComponentOptions = (component, taskStatus, movementModeCounts, selectorCounts) => {
-  if (component.data?.options && component.dynamicOptions) {
-    component.data.options = setupOptions(component, taskStatus, movementModeCounts, selectorCounts);
-  }
-  return component;
-};
-
-const setupFilterCounts = (filter, taskStatus, movementModeCounts, selectorCounts) => {
-  filter.pages = filter.pages.map((page) => {
-    return {
-      ...page,
-      components: page.components.map((component) => {
-        return setupComponentOptions(component, taskStatus, movementModeCounts, selectorCounts);
-      }),
-    };
-  });
-};
-
-const onGetComponent = (component, wrap) => {
-  if (component.type === 'select') {
-    const select = (
-      <select
-        className="govuk-select"
-        id={component.id}
-        name={component.fieldId}
-        onChange={component.onChange}
-        onBlur={component.onChange}
-        value={component.value}
-      >
-        {component.data.options.map((opt) => {
-          return (
-            <option
-              key={opt.value}
-              value={opt.value}
-            >
-              {opt.label}
-            </option>
-          );
-        })}
-      </select>
-    );
-    if (wrap) {
-      const attrs = Utils.Component.clean(component, ['fieldId', 'dynamicOptions']);
-      return Utils.Component.wrap(attrs, select);
-    }
-    return select;
-  }
-  return null;
-};
-
-const AirpaxFilter = ({ taskStatus, onApply, appliedFilters, movementModeCounts, selectorCounts }) => {
-  const onApplyFilter = async (_, payload, onSuccess) => {
-    onSuccess(payload);
-    onApply(payload);
-  };
-
-  setupFilterCounts(airpax, taskStatus, movementModeCounts, selectorCounts);
-
-  return (
-    <FormRenderer
-      {...airpax}
-      hooks={{
-        onGetComponent,
-        onSubmit: onApplyFilter,
-      }}
-      data={appliedFilters}
-    />
-  );
-};
-
-const RoRoFilter = ({ taskStatus, onApply, appliedFilters, movementModeCounts, selectorCounts }) => {
-  const onApplyFilter = async (_, payload, onSuccess) => {
-    onSuccess(payload);
-    onApply(payload);
-  };
-
-  setupFilterCounts(roro, taskStatus, movementModeCounts, selectorCounts);
-
-  return (
-    <FormRenderer
-      {...roro}
-      hooks={{
-        onGetComponent,
-        onSubmit: onApplyFilter,
-      }}
-      data={appliedFilters}
-    />
-  );
-};
 
 const getMovementSelectorCounts = (mode, filtersAndSelectorsCount) => {
   if (mode === MOVEMENT_VARIANT.RORO) {
     return {
       movementModeCounts: filtersAndSelectorsCount?.slice(0, 3),
-      selectorCounts: filtersAndSelectorsCount?.slice(3),
+      modeSelectorCounts: filtersAndSelectorsCount?.slice(3),
     };
   }
   return {
     movementModeCounts: filtersAndSelectorsCount?.slice(0, 1),
-    selectorCounts: filtersAndSelectorsCount?.slice(1),
+    modeSelectorCounts: filtersAndSelectorsCount?.slice(1),
   };
 };
 
-const Filter = ({ mode, taskStatus, onApply, appliedFilters, filtersAndSelectorsCount }) => {
+const Filter = ({ mode, taskStatus: _taskStatus, onApply, appliedFilters, filtersAndSelectorsCount, rulesOptions }) => {
   const [, updateState] = useState();
   const forceUpdate = useCallback(() => updateState({}), []);
-  const { movementModeCounts, selectorCounts } = getMovementSelectorCounts(mode, filtersAndSelectorsCount);
+  const { movementModeCounts, modeSelectorCounts } = getMovementSelectorCounts(mode, filtersAndSelectorsCount);
+
+  const getCountForOption = (fieldId, value, taskStatus, modeCounts, selectorCounts) => {
+    if (fieldId === 'hasSelectors') {
+      if (value === DEFAULT_APPLIED_RORO_FILTER_STATE.hasSelectors) {
+        value = null;
+      }
+      return selectorCounts?.find((f) => {
+        return f.filterParams[fieldId] === JSON.parse(value);
+      })?.statusCounts[taskStatus];
+    }
+    if (fieldId === 'selectors') {
+      return selectorCounts?.find((f) => {
+        return f.filterParams[fieldId] === value;
+      })?.statusCounts[taskStatus];
+    }
+    if (fieldId === 'mode') {
+      if (!value) {
+        return null;
+      }
+      return modeCounts?.find((f) => {
+        return f.filterParams.movementModes[0] === value;
+      })?.statusCounts[taskStatus];
+    }
+    return 0;
+  };
+
+  const setupOptions = (component, taskStatus, modeCounts, selectorCounts) => {
+    return component.data.options.map((opt) => {
+      let count = getCountForOption(component.fieldId, opt.value, taskStatus, modeCounts, selectorCounts);
+      count = count === undefined ? 0 : count;
+      const originalLabel = opt.originalLabel || opt.label;
+      const label = count === null ? originalLabel : `${originalLabel} (${count})`;
+      return {
+        ...opt,
+        count,
+        originalLabel,
+        label,
+      };
+    });
+  };
+
+  const setupComponentOptions = (component, taskStatus, modeCounts, selectorCounts) => {
+    if (component.data?.options && component.dynamicOptions) {
+      component.data.options = setupOptions(component, taskStatus, modeCounts, selectorCounts);
+    }
+    return component;
+  };
+
+  const setupFilterCounts = (filter, taskStatus, modeCounts, selectorCounts) => {
+    filter.pages = filter.pages.map((page) => {
+      return {
+        ...page,
+        components: page.components.map((component) => {
+          return setupComponentOptions(component, taskStatus, modeCounts, selectorCounts);
+        }),
+      };
+    });
+  };
+
+  const onGetComponent = (component, wrap) => {
+    const attrs = Utils.Component.clean(component, ['fieldId', 'dynamicOptions', 'multi']);
+    if (component.type === 'select') {
+      const select = (
+        <select
+          className="govuk-select"
+          id={component.id}
+          name={component.fieldId}
+          onChange={component.onChange}
+          onBlur={component.onChange}
+          value={component.value}
+        >
+          {component.data.options.map((opt) => {
+            return (
+              <option
+                key={opt.value}
+                value={opt.value}
+              >
+                {opt.label}
+              </option>
+            );
+          })}
+        </select>
+      );
+      if (wrap) {
+        return Utils.Component.wrap(attrs, select);
+      }
+      return select;
+    }
+    if (component.type === 'multiautocomplete') {
+      const multiSelect = (
+        <MultiSelectAutocomplete
+          className="hods-multi-select-autocomplete"
+          {...component}
+          item={{ value: 'id', label: 'name' }}
+          options={rulesOptions}
+        />
+      );
+      if (wrap) {
+        return Utils.Component.wrap(attrs, multiSelect);
+      }
+      return multiSelect;
+    }
+    return null;
+  };
+
+  const AirpaxFilter = () => {
+    const onApplyFilter = async (_, payload, onSuccess) => {
+      onSuccess(payload);
+      onApply(payload);
+    };
+
+    setupFilterCounts(airpax, _taskStatus, movementModeCounts, modeSelectorCounts);
+
+    return (
+      <FormRenderer
+        {...airpax}
+        hooks={{
+          onGetComponent,
+          onSubmit: onApplyFilter,
+        }}
+        data={appliedFilters}
+      />
+    );
+  };
+
+  const RoRoFilter = () => {
+    const onApplyFilter = async (_, payload, onSuccess) => {
+      onSuccess(payload);
+      onApply(payload);
+    };
+
+    setupFilterCounts(roro, _taskStatus, movementModeCounts, modeSelectorCounts);
+
+    return (
+      <FormRenderer
+        {...roro}
+        hooks={{
+          onGetComponent,
+          onSubmit: onApplyFilter,
+        }}
+        data={appliedFilters}
+      />
+    );
+  };
 
   useEffect(() => {
     forceUpdate();
@@ -158,24 +174,25 @@ const Filter = ({ mode, taskStatus, onApply, appliedFilters, filtersAndSelectors
   if (mode === MOVEMENT_VARIANT.RORO) {
     return (
       <RoRoFilter
-        taskStatus={taskStatus}
+        taskStatus={_taskStatus}
         onApply={onApply}
         appliedFilters={appliedFilters}
         filtersAndSelectorsCount={filtersAndSelectorsCount}
         movementModeCounts={movementModeCounts}
-        selectorCounts={selectorCounts}
+        selectorCounts={modeSelectorCounts}
       />
     );
   }
 
   return (
     <AirpaxFilter
-      taskStatus={taskStatus}
+      taskStatus={_taskStatus}
       onApply={onApply}
       appliedFilters={appliedFilters}
       filtersAndSelectorsCount={filtersAndSelectorsCount}
       movementModeCounts={movementModeCounts}
-      selectorCounts={selectorCounts}
+      selectorCounts={modeSelectorCounts}
+      rulesOptions={rulesOptions}
     />
   );
 };
