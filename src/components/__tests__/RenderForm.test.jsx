1e7c87d3d994df40ae213fbe96577c54f404f381
Charles Okafor <ckokafor@hotmail.com>
2022-06-10 19:00:28 +0100

    adds test to verify form is submitted

--- src/components/__tests__/RenderForm.test.jsx
+++ src/components/__tests__/RenderForm.test.jsx
@@ -1,8 +1,6 @@
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
 import '../../__mocks__/keycloakMock';
 
 import RenderForm from '../RenderForm';
@@ -11,15 +9,14 @@ import dismissTask from '../../cop-forms/dismissTaskCerberus';
 import { Renderers } from '../../utils/Form';
 
 describe('RenderForm', () => {
-  const mockAxios = new MockAdapter(axios);
-  const FUNCTION_CALLS = [];
+  const ON_CANCEL_CALLS = [];
 
   const ON_CANCEL = (action) => {
-    FUNCTION_CALLS.push(action);
+    ON_CANCEL_CALLS.push(action);
   };
 
   beforeEach(() => {
-    FUNCTION_CALLS.length = 0;
+    ON_CANCEL_CALLS.length = 0;
   });
 
   it('should call the cancel function when the cancel button is clicked', () => {
@@ -32,7 +29,21 @@ describe('RenderForm', () => {
 
     userEvent.click(screen.getByRole('button', { name: 'Cancel' }));
 
-    expect(FUNCTION_CALLS).toHaveLength(1);
-    expect(mockAxios.history.post).toHaveLength(1);
+    expect(ON_CANCEL_CALLS).toHaveLength(1);
+  });
+
+  it('should submit the form', () => {
+    render(<RenderForm
+      form={dismissTask}
+      onSubmit={jest.fn()}
+      onCancel={ON_CANCEL}
+      renderer={Renderers.REACT}
+    />);
+
+    userEvent.click(screen.getByRole('radio', { name: 'Vessel arrived' }));
+    userEvent.click(screen.getByRole('button', { name: 'Next' }));
+    userEvent.click(screen.getByRole('button', { name: 'Submit form' }));
+
+    expect(ON_CANCEL_CALLS).toHaveLength(0);
   });
 });
