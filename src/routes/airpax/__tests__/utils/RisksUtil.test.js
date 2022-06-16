931fe26e013d33932db53629906c3dbb3c60add2
chrisa105 <chris.allen888@gmail.com>
2022-06-16 08:38:42 +0100

    refactors code to risks util and adds test for risks util

--- src/routes/airpax/__tests__/utils/RisksUtil.test.js
+++ src/routes/airpax/__tests__/utils/RisksUtil.test.js
@@ -265,11 +265,219 @@ describe('RisksUtil', () => {
   it('should return null if rules matches is null', () => {
     const rules = {
       risks: {
-        matchedRules: [],
+        matchedRules: null,
       },
     };
 
-    const output = RisksUtil.getMatches(rules);
+    const output = RisksUtil.getRules(rules);
+    expect(output).toEqual(null);
+  });
+
+  it('should get target task highestThreatLevel if present', () => {
+    const highestThreatLevel = {
+      risks: {
+        highestThreatLevel: {
+          type: 'SELECTOR',
+          value: 'B',
+        },
+      },
+    };
+
+    const expected = { 'type': 'SELECTOR', 'value': 'B' };
+
+    const output = RisksUtil.getHighestThreat(highestThreatLevel);
+    expect(output).toEqual(expected);
+  });
+
+  it('should return null if target task highestThreatLevel is null', () => {
+    const highestThreatLevel = {
+      risks: {
+        highestThreatLevel: null,
+      },
+    };
+
+    const output = RisksUtil.getHighestThreat(highestThreatLevel);
+    expect(output).toEqual(null);
+  });
+
+  it('should get target task matchedSelectorGroups if present', () => {
+    const selectors = {
+      risks: {
+        matchedSelectorGroups: {
+          groups: [
+            {
+              groupReference: 'SR-245',
+              groupVersionNumber: 1,
+              requestingOfficer: 'fe',
+              intelligenceSource: 'fefe',
+              category: 'A',
+              threatType: 'Class A Drugs',
+              pointOfContactMessage: 'fdvdfb',
+              pointOfContact: 'bfb',
+              inboundActionCode: 'No action required',
+              outboundActionCode: 'No action required',
+              notes: 'notes',
+              creator: 'user',
+              selectors: [
+                {
+                  id: 279,
+                  reference: '2021-279',
+                  category: 'A',
+                  warning: {
+                    status: 'YES',
+                    types: [
+                      'VIOLENCE',
+                      'FIREARMS',
+                      'OTHER',
+                    ],
+                    detail: 'other warning details',
+                  },
+                  indicatorMatches: [
+                    {
+                      entity: 'Message',
+                      descriptor: 'mode',
+                      operator: 'in',
+                      value: 'RORO Accompanied Freight',
+                    },
+                    {
+                      entity: 'Trailer',
+                      descriptor: 'registrationNumber',
+                      operator: 'equal',
+                      value: 'qwerty',
+                    },
+                  ],
+                  description: 'RORO Accompanied Freight qwerty',
+                },
+                {
+                  id: 300,
+                  reference: '2022-300',
+                  category: 'B',
+                  warning: {
+                    status: 'NO',
+                    types: [],
+                    detail: null,
+                  },
+                  indicatorMatches: [
+                    {
+                      entity: 'Trailer',
+                      descriptor: 'registrationNumber',
+                      operator: 'equal',
+                      value: 'GB09NFD',
+                    },
+                  ],
+                  description: 'GB09NFD',
+                },
+              ],
+            },
+          ],
+          totalNumberOfSelectors: 2,
+        },
+      },
+    };
+
+    const expected = { 'groups':
+      [
+        { 'groupReference': 'SR-245',
+          'groupVersionNumber': 1,
+          'requestingOfficer': 'fe',
+          'intelligenceSource': 'fefe',
+          'category': 'A',
+          'threatType': 'Class A Drugs',
+          'pointOfContactMessage': 'fdvdfb',
+          'pointOfContact': 'bfb',
+          'inboundActionCode': 'No action required',
+          'outboundActionCode': 'No action required',
+          'notes': 'notes',
+          'creator': 'user',
+          'selectors': [
+            { 'id': 279,
+              'reference': '2021-279',
+              'category': 'A',
+              'warning': { 'status': 'YES', 'types': ['VIOLENCE', 'FIREARMS', 'OTHER'], 'detail': 'other warning details' },
+              'indicatorMatches': [{ 'entity': 'Message', 'descriptor': 'mode', 'operator': 'in', 'value': 'RORO Accompanied Freight' }, { 'entity': 'Trailer', 'descriptor': 'registrationNumber', 'operator': 'equal', 'value': 'qwerty' }],
+              'description': 'RORO Accompanied Freight qwerty' },
+            { 'id': 300,
+              'reference': '2022-300',
+              'category': 'B',
+              'warning': { 'status': 'NO', 'types': [], 'detail': null },
+              'indicatorMatches': [{ 'entity': 'Trailer', 'descriptor': 'registrationNumber', 'operator': 'equal', 'value': 'GB09NFD' }],
+              'description': 'GB09NFD' },
+          ] },
+      ],
+    'totalNumberOfSelectors': 2 };
+
+    const output = RisksUtil.getMatchedSelectorGroups(selectors);
+    expect(output).toEqual(expected);
+  });
+
+  it('should return null if target task matchedSelectorGroups is null', () => {
+    const selectors = {
+      risks: {
+        matchedSelectorGroups: null,
+      },
+    };
+
+    const output = RisksUtil.getMatchedSelectorGroups(selectors);
+    expect(output).toEqual(null);
+  });
+
+  it('should get target task matchedRules if present', () => {
+    const rules = {
+      risks: {
+        matchedRules: [
+          {
+            id: 535,
+            name: 'Selector Matched Rule',
+            type: 'Both',
+            priority: 'Tier 1',
+            agency: '',
+            description: 'Test Description',
+            version: 1,
+            abuseTypes: [
+              'National Security at the Border',
+            ],
+            indicatorMatches: [
+              {
+                entity: 'Message',
+                descriptor: 'mode',
+                operator: 'in',
+                value: '[RORO Accompanied Freight, RORO Tourist, RORO Unaccompanied Freight]',
+              },
+              {
+                entity: 'Message',
+                descriptor: 'selectorsMatched',
+                operator: 'equal',
+                value: 'true',
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    const expected = [
+      { 'abuseTypes': ['National Security at the Border'],
+        'agency': '',
+        'description': 'Test Description',
+        'id': 535,
+        'indicatorMatches': [{ 'descriptor': 'mode', 'entity': 'Message', 'operator': 'in', 'value': '[RORO Accompanied Freight, RORO Tourist, RORO Unaccompanied Freight]' }, { 'descriptor': 'selectorsMatched', 'entity': 'Message', 'operator': 'equal', 'value': 'true' }],
+        'name': 'Selector Matched Rule',
+        'priority': 'Tier 1',
+        'type': 'Both',
+        'version': 1 }];
+
+    const output = RisksUtil.getMatchedRules(rules);
+    expect(output).toEqual(expected);
+  });
+
+  it('should return null if target task matchedRules is null', () => {
+    const rules = {
+      risks: {
+        matchedRules: null,
+      },
+    };
+
+    const output = RisksUtil.getMatchedRules(rules);
     expect(output).toEqual(null);
   });
 });
--- src/routes/airpax/utils/risksUtil.jsx
+++ src/routes/airpax/utils/risksUtil.jsx
@@ -92,6 +92,96 @@ const getMatchedRules = (version) => {
   return null;
 };
 
+const hasHighestThreatLevel = (targetTask) => {
+  return !!targetTask?.risks?.highestThreatLevel;
+};
+
+const getHighestThreatLevel = (targetTask) => {
+  if (hasHighestThreatLevel(targetTask)) {
+    return targetTask.risks.highestThreatLevel;
+  }
+  return null;
+};
+
+const hasRiskMatchedSelectorGroups = (targetTask) => {
+  return !!targetTask?.risks?.matchedSelectorGroups;
+};
+
+const getRiskMatchedSelectorGroups = (targetTask) => {
+  if (hasRiskMatchedSelectorGroups(targetTask)) {
+    return targetTask.risks.matchedSelectorGroups;
+  }
+  return null;
+};
+
+const hasRiskMatchedRules = (targetTask) => {
+  return !!targetTask?.risks?.matchedRules;
+};
+
+const getRiskMatchedRules = (targetTask) => {
+  if (hasRiskMatchedRules(targetTask)) {
+    return targetTask.risks.matchedRules;
+  }
+  return null;
+};
+
+const extractRiskType = (targetTask, highestRisk) => {
+  const riskType = [];
+  if (highestRisk.type.toLowerCase() === 'selector') {
+    const selectors = [getRiskMatchedSelectorGroups(targetTask).groups];
+    selectors.forEach((selector) => {
+      Object.values(selector).every((s) => {
+        if (s.category.toLowerCase() === (highestRisk.value.toLowerCase())) {
+          riskType.push(s.threatType);
+        }
+      });
+    });
+  } else {
+    const rules = [getRiskMatchedRules(targetTask)];
+    rules.forEach((rule) => {
+      Object.values(rule).every((r) => {
+        if (r.priority.toLowerCase() === (highestRisk.value.toLowerCase())) {
+          riskType.push(r.abuseTypes[0]);
+        }
+      });
+    });
+  }
+  return riskType[0];
+};
+
+const formatTargetRisk = (targetTask, highestThreatLevel) => {
+  const risksRules = getRiskMatchedRules(targetTask).length + getRiskMatchedSelectorGroups(targetTask).totalNumberOfSelectors;
+  if (highestThreatLevel) {
+    const topRisk = extractRiskType(targetTask, highestThreatLevel);
+    const count = risksRules > 0 && risksRules - 1;
+    return `${topRisk} and ${pluralise.withCount(
+      count,
+      '% other rule',
+      '% other rules',
+    )}`;
+  }
+  return null;
+};
+
+const formatHighestThreatLevel = (targetTask) => {
+  const highestThreatLevel = getHighestThreatLevel(targetTask);
+  if (!highestThreatLevel) {
+    return;
+  }
+  return (
+    <h4 className="govuk-heading-s task-highest-risk">
+      {highestThreatLevel.type.toLowerCase() === 'rule' ? '' : highestThreatLevel.type}
+      {' '}
+      <span className="govuk-tag govuk-tag--riskTier">
+        {highestThreatLevel.value}
+      </span>
+      <span className="govuk-body task-risk-statement">
+        {formatTargetRisk(targetTask, highestThreatLevel)}
+      </span>
+    </h4>
+  );
+};
+
 const RisksUtil = {
   getRisks: getRisk,
   format: formatTargetIndicators,
@@ -100,8 +190,24 @@ const RisksUtil = {
   getMatches: getIndicatorMatches,
   getGroups: getSelectorGroups,
   getRules: getMatchedRules,
+  getHighestThreat: getHighestThreatLevel,
+  getMatchedSelectorGroups: getRiskMatchedSelectorGroups,
+  getMatchedRules: getRiskMatchedRules,
+  formatHighestThreat: formatHighestThreatLevel,
 };
 
 export default RisksUtil;
 
-export { getRisk, formatTargetIndicators, getTargetingIndicators, getSelectorWarning, getIndicatorMatches, getSelectorGroups, getMatchedRules };
+export {
+  getRisk,
+  formatTargetIndicators,
+  getTargetingIndicators,
+  getSelectorWarning,
+  getIndicatorMatches,
+  getSelectorGroups,
+  getMatchedRules,
+  getHighestThreatLevel,
+  getRiskMatchedSelectorGroups,
+  getRiskMatchedRules,
+  formatHighestThreatLevel,
+};
