6aeeb29bf5641a343e0b6604f84cd773f96beb6c
chrisa105 <chris.allen888@gmail.com>
2022-06-27 12:26:21 +0100

    bug fix for multiple notes being saved (airpax)

--- src/components/__tests__/TaskNotes.test.jsx
+++ src/components/__tests__/TaskNotes.test.jsx
@@ -202,7 +202,7 @@ describe('TaskNotes', () => {
     expect(screen.queryByText('Add a new note')).not.toBeInTheDocument();
   });
 
-  it('should add a note an submit the form (airpax)', async () => {
+  it('should add a note on submit the form (airpax)', async () => {
     const input = '\nthis \\is a "test" \nnote';
     const expectedPayload = '\\nthis \\\\is a \\"test\\" \\nnote';
 
@@ -224,5 +224,43 @@ describe('TaskNotes', () => {
     const requestPayload = mockAxios.history.post[0].data;
     const parsedPayload = JSON.parse(requestPayload)[0].content;
     expect(parsedPayload).toEqual(expectedPayload);
+  });
+
+  it('should add multiple notes on multiple submit the form (airpax)', async () => {
+    const firstinput = '\nthis \\is a "test" \nnote';
+    const secondinput = '\nthis \\is another "test" \nnote';
+    const firstExpectedPayload = '\\nthis \\\\is a \\"test\\" \\nnote';
+    const secondExpectedPayload = '\\nthis \\\\is another \\"test\\" \\nnote';
+
+    const { container } = await waitFor(() => render(<TaskNotes
+      noteVariant={MOVEMENT_VARIANT.AIRPAX}
+      displayForm
+      businessKey="ghi"
+      setRefreshNotesForm={jest.fn()}
+    />));
+
+    // Type into the textarea...
+    const firsttextarea = container.getElementsByClassName('govuk-textarea')[0];
+    fireEvent.change(firsttextarea, { target: { name: 'note', value: firstinput } });
+
+    // ... and then click on the submit button.
+    const submit = container.getElementsByClassName('hods-button')[0];
+    await waitFor(() => userEvent.click(submit));
+
+    // Type into the textarea again...
+    const secondtextarea = container.getElementsByClassName('govuk-textarea')[0];
+    fireEvent.change(secondtextarea, { target: { name: 'note', value: secondinput } });
+
+    // ... and then click on the submit button. again
+    const secondsubmit = container.getElementsByClassName('hods-button')[0];
+    await waitFor(() => userEvent.click(secondsubmit));
+
+    const firstRequestPayload = mockAxios.history.post[0].data;
+    const firstParsedPayload = JSON.parse(firstRequestPayload)[0].content;
+    expect(firstParsedPayload).toEqual(firstExpectedPayload);
+
+    const secondRequestPayload = mockAxios.history.post[1].data;
+    const secondParsedPayload = JSON.parse(secondRequestPayload)[0].content;
+    expect(secondParsedPayload).toEqual(secondExpectedPayload);
   });
 });
