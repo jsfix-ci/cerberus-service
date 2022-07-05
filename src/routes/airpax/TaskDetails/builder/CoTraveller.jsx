4f92b77807399ce8e9fb1431409d293c9f4c4d3d
Charles Okafor <ckokafor@hotmail.com>
2022-07-05 18:25:13 +0100

    removes columns from co-travellers table

--- src/routes/airpax/TaskDetails/builder/CoTraveller.jsx
+++ src/routes/airpax/TaskDetails/builder/CoTraveller.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import * as pluralise from 'pluralise';
-import { Link, VisuallyHidden } from '@ukhomeoffice/cop-react-components';
 import Table from '../../../../components/Table';
 
-import { CO_TRAVELLERS_TABLE_HEADERS,
-  LONG_DATE_FORMAT, DAYJS_FUTURE,
-  DAYJS_FUTURE_AIXPAX_REPLACE,
-  UNKNOWN_TEXT } from '../../../../constants';
+import { CO_TRAVELLERS_TABLE_HEADERS } from '../../../../constants';
 
-import { PersonUtil, MovementUtil, BookingUtil, DateTimeUtil, DocumentUtil, BaggageUtil } from '../../utils';
-import { calculateTimeDifference } from '../../../../utils/DatetimeUtil';
+import { PersonUtil, MovementUtil, DocumentUtil } from '../../utils';
 import '../../../../components/__assets__/Table.scss';
-
-const isValid = (value) => {
-  return value !== UNKNOWN_TEXT && value;
-};
 
 const toTravellerColumnContent = (person, version) => {
   return (
@@ -36,34 +27,6 @@ const toAgeColumnContent = (person) => {
   );
 };
 
-const toCheckinColumnContent = (version) => {
-  const booking = BookingUtil.get(version);
-  const journey = MovementUtil.movementJourney(version);
-  const checkInAt = BookingUtil.checkInAt(booking);
-  const departureTime = MovementUtil.departureTime(journey);
-  return (
-    <>
-      <div className="font__bold">
-        {isValid(checkInAt) ? DateTimeUtil.format(checkInAt, LONG_DATE_FORMAT) : UNKNOWN_TEXT}
-      </div>
-      <div className="font__light">
-        {(isValid(checkInAt) && isValid(departureTime))
-          ? calculateTimeDifference(DateTimeUtil.toList(checkInAt, departureTime)).replace(
-            DAYJS_FUTURE,
-            DAYJS_FUTURE_AIXPAX_REPLACE,
-          ) : UNKNOWN_TEXT}
-      </div>
-    </>
-  );
-};
-
-const toSeatColumnContent = (version) => {
-  const seatNumber = MovementUtil.seatNumber(MovementUtil.movementFlight(version), true);
-  return (
-    <div className="font__bold">{seatNumber}</div>
-  );
-};
-
 const toDocumentColumnContent = (person) => {
   const document = DocumentUtil.get(person);
   return (
@@ -74,44 +37,13 @@ const toDocumentColumnContent = (person) => {
   );
 };
 
-const toBaggageColumnContent = (version) => {
-  const baggage = BaggageUtil.get(version);
-  const checkedBags = BaggageUtil.formatCheckedCount(baggage);
-  const checkedWeight = BaggageUtil.weight(baggage);
-  return (
-    <>
-      <div className="font__bold">
-        {(checkedBags !== UNKNOWN_TEXT && checkedWeight !== UNKNOWN_TEXT)
-          ? (BaggageUtil.formatCheckedCount(baggage), BaggageUtil.weight(baggage)) : UNKNOWN_TEXT}
-      </div>
-    </>
-  );
-};
-
-// Update this once we start receiving data
-const toMovementDetailsColumnContent = (index) => {
-  return (
-    <>
-      {index === 0 && <div className="font__light">This movement</div>}
-      {/* Line below has been intentionally hidden */}
-      <VisuallyHidden>
-        {index !== 0 && <Link href="/airpax/tasks" target="_blank">Movement detail</Link>}
-      </VisuallyHidden>
-    </>
-  );
-};
-
 const toRows = (allPersons, version) => {
   const rows = [];
-  allPersons.map((person, index) => {
+  allPersons.map((person) => {
     rows.push([
       toTravellerColumnContent(person, version),
       toAgeColumnContent(person),
-      toCheckinColumnContent(version),
-      toSeatColumnContent(version),
       toDocumentColumnContent(person),
-      toBaggageColumnContent(version),
-      toMovementDetailsColumnContent(index),
     ]);
   });
   return rows;
--- src/routes/airpax/__tests__/builder/CoTraveller.test.jsx
+++ src/routes/airpax/__tests__/builder/CoTraveller.test.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import CoTraveller from '../../TaskDetails/builder/CoTraveller';
 import taskDetailsData from '../../__fixtures__/taskData_AirPax_TaskDetails.fixture.json';
 
 describe('CoTraveller', () => {
   it('should render the co traveller component', () => {
-    render(<CoTraveller version={taskDetailsData} />);
-    expect(screen.getByText('1 traveller')).toBeInTheDocument();
-    expect(screen.getAllByText('Traveller')).toHaveLength(2);
-    expect(screen.getAllByText('Age')).toHaveLength(2);
-    expect(screen.getAllByText('Check-in')).toHaveLength(2);
-    expect(screen.getAllByText('Seat')).toHaveLength(2);
-    expect(screen.getAllByText('Document')).toHaveLength(2);
-    expect(screen.getAllByText('Checked baggage')).toHaveLength(2);
-    expect(screen.getByText('1kg')).toBeInTheDocument();
-    expect(screen.getByText('Passport')).toBeInTheDocument();
-    expect(screen.getByText('Issued by FR')).toBeInTheDocument();
-    expect(screen.getByText(/FORD/)).toBeInTheDocument();
-    expect(screen.getByText(/Isaiah/)).toBeInTheDocument();
-    expect(screen.getByText(/DC/)).toBeInTheDocument();
-    expect(screen.getByText(/Male/)).toBeInTheDocument();
-    expect(screen.getByText(/13 May 1966/)).toBeInTheDocument();
-    expect(screen.getByText(/United Kingdom \(GBR\)/)).toBeInTheDocument();
+    const tree = renderer.create(<CoTraveller version={taskDetailsData.versions[0]} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
