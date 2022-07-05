ce289de6f32bd3ceb8fc8794df7fc709917a6c40
Charles Okafor <ckokafor@hotmail.com>
2022-07-05 17:31:31 +0100

    removes main person from the co-travellers table

--- src/routes/airpax/__tests__/utils/PersonUtil.test.js
+++ src/routes/airpax/__tests__/utils/PersonUtil.test.js
@@ -355,4 +355,40 @@ describe('PersonUtil', () => {
     const output = PersonUtil.allPersons(person, coTravellers);
     expect(output.length).toEqual(4);
   });
+
+  it('should return the count of all co-travellrs within the movement', () => {
+    const targetTaskMin = {
+      movement: {
+        otherPersons: [...coTravellers],
+      },
+    };
+    expect(PersonUtil.othersCount(targetTaskMin)).toEqual(3);
+  });
+
+  it('should return 0 when co-travellrs is null within the movement', () => {
+    const targetTaskMin = {
+      movement: {
+        otherPersons: null,
+      },
+    };
+    expect(PersonUtil.othersCount(targetTaskMin)).toEqual(0);
+  });
+
+  it('should return 0 when co-travellrs is undefined within the movement', () => {
+    const targetTaskMin = {
+      movement: {
+        otherPersons: undefined,
+      },
+    };
+    expect(PersonUtil.othersCount(targetTaskMin)).toEqual(0);
+  });
+
+  it('should return 0 when co-travellrs is an empty array within the movement', () => {
+    const targetTaskMin = {
+      movement: {
+        otherPersons: [],
+      },
+    };
+    expect(PersonUtil.othersCount(targetTaskMin)).toEqual(0);
+  });
 });
