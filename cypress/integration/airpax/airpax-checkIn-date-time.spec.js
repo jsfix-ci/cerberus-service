e7e7ca2624497dbbeec9c24f53730e6d66289237
mitchelegboko <mitchel.egboko@identitye2e.com>
2022-07-11 17:26:52 +0100

    Added test to verify CheckIn dateTime is displayed

--- cypress/integration/airpax/airpax-checkIn-date-time.spec.js
+++ cypress/integration/airpax/airpax-checkIn-date-time.spec.js
@@ -3,8 +3,7 @@ describe('Targeter to see how long before departure check-in occurs So that Targ
     cy.login(Cypress.env('userName'));
     cy.acceptPNRTerms();
   });
-
-  it('Should verify When check-in time is not available, it populates as Unknown in Cerberus UI', () => {
+  it('Should verify when check-in time is not available, it populates as Unknown in Cerberus UI', () => {
     const taskName = 'AIRPAX';
     cy.fixture('airpax/task-airpax.json').then((task) => {
       task.data.movement.serviceMovement.features.feats['STANDARDISED:checkinDateTime'].value = null;
@@ -15,6 +14,21 @@ describe('Targeter to see how long before departure check-in occurs So that Targ
         let businessKey = taskResponse.id;
         cy.checkAirPaxTaskDisplayed(businessKey);
         cy.verifyAirPaxCheckInDateTime('Unknown');
+      });
+    });
+  });
+  
+  it('Should verify when check-in time is displayed', () => {
+    const taskName = 'AIRPAX';
+    cy.fixture('airpax/task-airpax.json').then((task) => {
+      task.data.movement.serviceMovement.features.feats['STANDARDISED:checkinDateTime'].value = '2022-06-10T09:00:00Z';
+      task.data.movementId = `${taskName}_${Math.floor((Math.random() * 1000000) + 1)}:CMID=TEST`;
+      cy.createTargetingApiTask(task).then((taskResponse) => {
+        expect(taskResponse.movement.id).to.contain('AIRPAX');
+        cy.wait(4000);
+        let businessKey = taskResponse.id;
+        cy.checkAirPaxTaskDisplayed(businessKey);
+        cy.verifyAirPaxCheckInDateTime('10 Jun 2022 at 09:00');
       });
     });
   });
